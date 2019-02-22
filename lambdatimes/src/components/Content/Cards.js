8febb2a53dd974d1dcff78ef936fe14b38bb1019
Frank Martinez <frankie.martinez@gmail.com>
2019-02-22 11:13:40 -0800

    Card, tab, data, all functionality working

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,14 +1,20 @@
 import React from 'react';
 // import React, { Component } from 'react';
-// import Card from './Card';
+import Card from './Card';
 
+
+// Card keys are authors name + random generated number
 const Cards = props => {
-  console.log(props); // Tab selection changes card array.
+  // console.log(props); // Tab selection changes card array.
   
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+          new Card component for each passing the card as the only prop */}
+      {props.cards.map(card => (
+        <Card key={`${card.author}#${parseInt((Math.random()*10000),0)}`} card={card} />
+        
+      ))}
     </div>
   )
 }
